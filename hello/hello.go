de784954eff136a48f3bb92983c2e71b2b89579f
Jignesh Patel <jigneshp@MML784.local>
2020-10-22 15:36:34 +0100

    added french greeting

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello will use the parameters to welcome the user
 func Hello(name string, language string) string {
@@ -15,9 +17,13 @@ func Hello(name string, language string) string {
 	if language == spanish {
 		return spanishHelloPrefix + name + "!"
 	}
+
+	if language == french {
+		return frenchHelloPrefix + name + "!"
+	}
 	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("Name", ""))
+	fmt.Println(Hello("Name", "Language"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -29,4 +29,10 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("saying hello to people in French", func(t *testing.T) {
+		got := Hello("Reggie", "French")
+		want := "Bonjour, Reggie!"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
