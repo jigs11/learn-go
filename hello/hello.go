df02550071cf9991003cbfb8ce742d0c01c03b4a
Jignesh Patel <jigneshp@MML784.local>
2020-10-22 15:11:19 +0100

    say hello in Spanish

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 // Hello will use the parameters to welcome the user
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name + "!"
 	}
 	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("Name"))
+	fmt.Println(Hello("Name", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,14 +12,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jigs")
+		got := Hello("Jigs", "")
 		want := "Hello, Jigs!"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people in Spanish", func(t *testing.T) {
+		got := Hello("Rosie", "Spanish")
+		want := "Hola, Rosie!"
 		assertCorrectMessage(t, got, want)
 	})
 
